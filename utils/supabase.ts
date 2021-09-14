44af22a911ef8184decae5b2495f9459d0e7c3f6
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2021-09-13 23:32:35 -0300

    Initial commit

---
+++ utils/supabase.ts
@@ -0,0 +1,6 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = new SupabaseClient(supabaseUrl, supabaseKey);
